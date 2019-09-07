8e9c0e9e04200c00af4065a991cc56b06598c615
kishor <kishor@KISHOR-PC>
2019-09-07 13:44:36 +0530

    Add project files.

---
+++ imagetask/Controllers/ImageController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace imagetask.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Image
+        public ActionResult showimg()
+        {
+            string imgpath = Server.MapPath("~/content/Image/images-02.jpeg");
+            byte[] bytedata = System.IO.File.ReadAllBytes(imgpath);
+            string imgBase64Data = Convert.ToBase64String(bytedata);
+            string imgDataURL = string.Format("data:image/jpg;base64,{0}", imgBase64Data);
+            ViewBag.ImageData = imgDataURL;
+            return View();
+        
+            
+        }
+    }
+}
